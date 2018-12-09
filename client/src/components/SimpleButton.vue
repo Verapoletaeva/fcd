1301560a0aa3858c15a3b0c6e5dfe76d65a178de
Vera <poletaeva.vera835@yandex.ru>
2018-12-09 20:13:32 +0300

    Добавлен компонент simple-button

---
+++ client/src/components/SimpleButton.vue
@@ -0,0 +1,64 @@
+<template>
+  <button
+      :class="['btn', className]"
+      @click="$emit('click', $event.target.value)">
+    <slot/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'simpleButton',
+  props: {
+     color: String
+  },
+  computed: {
+     className: function() {
+        if (this.color === "blue" || this.color === "red")
+           return (this.color + "Btn");
+        return "blueBtn"
+     }
+  }
+}
+</script>
+
+<style lang="scss">
+.btn {
+  transition: background-color 0.1s linear;
+
+  display: inline-block;
+
+  outline: none;
+  border: 1px solid #64bab7;
+  border-radius: 10px;
+  box-shadow: 4px 4px 7px rgba(100, 186, 183, 0.75),
+  inset 1px 1px 2px white,
+  inset -1px -1px 2px black;
+
+  width: 150px;
+  height: 40px;
+
+  font: 20px Arial;
+  color: white;
+
+  text-align: center;
+  user-select: none;
+
+  &:active {
+    box-shadow: 4px 4px 7px rgba(100, 186, 183, 0.75),
+    inset 1px 1px 2px black;
+  }
+
+  &:hover {
+    /*FIXME*/
+  }
+}
+
+.blueBtn {
+  background: linear-gradient(to top, #087d78 0%,  #64BAB7 100%);
+}
+
+.redBtn {
+  background: linear-gradient(to top, #A51F29 0%,  #FF616C 100%);
+}
+</style>
--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -4,14 +4,18 @@
     <vButton @click="$emit('changePage', '/random')">Random</vButton>
     <vButton @click="$emit('changePage', '/auth')">Auth</vButton>
     <vButton @click="$emit('changePage', '/404')">404</vButton>
+    <div>Пример использования simpleButton</div>
+    <simple-button @click="" color="red">Оплатить</simple-button>
+    <simple-button @click="$emit('changePage', '/404')" color="blue">404</simple-button>
   </div>
 </template>
 
 <script>
 import vButton from '../components/vButton'
+import simpleButton from '../components/SimpleButton'
 export default {
   components: {
-    vButton
+    vButton, simpleButton
   }
 }
 </script>
